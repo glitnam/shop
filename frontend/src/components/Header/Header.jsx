2db2b9387bdcdf6ea402594b7791cc70d54f796b
Nam <nhunam2k2@gmail.com>
2024-08-03 15:49:32 +0700

    2

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Choose from a diverse menu feauturing a delectable array of dishes crafted with the finest ingrefients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining expertise, onr delicious meal at a time</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
